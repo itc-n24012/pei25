ada8bba86842debb46cccfe229741c2a06e9db80
itc-n24012 <n24012@std.it-college.ac.jp>
2025-10-10 02:59:56 +0000

    18の課題

--- m1_q4_18kadai.py
+++ m1_q4_18kadai.py
@@ -4,4 +4,5 @@ for p in phrase:
     if p not in list_p:
         list_p.append(p)
 print(len(phrase) - len(list_p))
-print("".join(list_p))
+print("".join(list_p)) #リストの中身を1つの文字列に戻す。
+#""は区切り文字がないことを意味するから
