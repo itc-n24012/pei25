b54e33d65fe5a09354d2740e93a2b35dd2ea9261
itc-n24012 <n24012@std.it-college.ac.jp>
2025-10-24 03:01:53 +0000

    1024課題修正

--- m1_q5_26kadai.py
+++ m1_q5_26kadai.py
@@ -1,6 +1,6 @@
-a = 2
-list_a = []
-for i in range(6):
-    list_a.append(a)
-    a *= 2
-print(list_a)
+a = 2 # 2を代入
+list_a = [] # 空のリスト
+for i in range(6): # 6回繰り返す
+    list_a.append(a) # 今のaの値をリストに追加
+    a *= 2 # aを2倍してループする
+print(list_a) # printする
