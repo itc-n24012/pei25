b23074965e5dbcaaa47bc7784dc8d547ea424c6d
itc-n24012 <n24012@std.it-college.ac.jp>
2025-11-21 01:00:23 +0000

    1121bool関数の課題

--- m2/m2_q4_24kadai.py
+++ m2/m2_q4_24kadai.py
@@ -1,12 +1,12 @@
 def func(a, b):
-    if a ** b <= 64:
-        return 1
-    else:
-        return 0
+    if a ** b <= 64: # aのb乗が64以下の場合
+        return 1 # 1を返す
+    else: # それ以外の場合
+        return 0 # 0を返す
 
-x = func(4, 3)
-y = func(3, 4)
-a = func(5, 6)
+x = func(4, 3) # 4の3乗は64なので1となる
+y = func(3, 4) # 3の4乗は81なので0となる
+a = func(5, 6) # 5の6乗は大きい数
 
 z = [bool(x), bool(y), bool(a)]
 print(z)
