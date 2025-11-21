df1548b02c2da8a72a11d83ca6c7b932a435dad2
itc-n24012 <n24012@std.it-college.ac.jp>
2025-11-21 00:47:50 +0000

    1114の問題

---
+++ m2/m2_q4_22.py
@@ -0,0 +1,12 @@
+list_a = ['Yokohama', 'Osaka', 'Nagoya', 'Sapporo', 'Fukuoka']
+list_b = []
+list_c = []
+list_d = []
+for name in list_a:
+    if len(name) > 6:
+        list_b.append(name)
+    elif name.count('a') > 1:
+        list_c.append(name)
+    else:
+        list_d.append(name)
+print(list_c)
