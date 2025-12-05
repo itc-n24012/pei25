008554181e7ee281231a06800c5736563dcfa80c
itc-n24012 <n24012@std.it-college.ac.jp>
2025-12-05 00:41:06 +0000

    1205値を出力する課題

---
+++ m3/m3_q3kadai.py
@@ -0,0 +1,10 @@
+list_a = [-4, 5, 6, 'Hello', 'World', 7, -8, 9] # list_aを作成
+
+print(abs(list_a[6])) # list_a[6]の絶対値
+
+print(bool(list_a[0] + list_a[1])) # list_a[0] + list_a[1]のbool値
+
+print(bin(list_a[2])[2:]) # list_a[2]を2進数にして0bを外す
+
+print(list_a[3] + list_a[4]) # list_a[3]とlist_a[4]を文字列連結
+
