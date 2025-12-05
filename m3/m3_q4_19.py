f1e9ff8995452ea8509563ea745fea0f7acf69fc
itc-n24012 <n24012@std.it-college.ac.jp>
2025-12-05 00:57:20 +0000

    1205問題

--- m3/m3_q4_19.py
+++ m3/m3_q4_19.py
@@ -1,6 +1,6 @@
-list_a = []
-for x in range(1, 10):
-    if x % 2 == 1:
+list_a = [] # list_aを作成
+for x in range(1, 10): # 1から9までの整数をループさせる
+    if x % 2 == 1: # 2で割った余りが1
         continue
-    list_a.append(x)
-print(list_a)
+    list_a.append(x) # xが偶数の場合、list_aの末尾にxを追加する
+print(list_a) # list_aを出力
