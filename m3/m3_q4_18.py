a82c38859e6bf230833833b06387d790dde6baf9
itc-n24012 <n24012@std.it-college.ac.jp>
2025-12-05 01:08:23 +0000

    1205問題

--- m3/m3_q4_18.py
+++ m3/m3_q4_18.py
@@ -1,4 +1,4 @@
-colors = '赤:青:黄'
-parts = colors.split(':')
-number = len(colors)
-print(parts[1] + number * parts[2])
+colors = '赤:青:黄' # 赤、青、黄を代入する
+parts = colors.split(':') # 結果をリストpartsに代入
+number = len(colors) # 結果をnumberに代入する
+print(parts[1] + number * parts[2]) # 結果を出力
